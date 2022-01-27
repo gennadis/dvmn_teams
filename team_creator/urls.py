13131e28aa2d37e2ef034a3a2da08b9b11504888
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-27 17:49:02 +0300

    Add time slots view template

--- team_creator/urls.py
+++ team_creator/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("<int:student_id>/", views.student_detail, name="detail"),
     # ex: /student/5/timeslot/
-    path("<int:student_id>/timeslot/", views.student_time_slots, name="time slots"),
+    # path("<int:student_id>/timeslot/", views.student_time_slots, name="time slots"),
     # ex: /student/5/team/
     path("<int:student_id>/team/", views.student_team, name="team"),
+    path("<int:student_id>/timeslot/", views.student_time_slots, name="time slots"),
 ]
--- team_creator/views.py
+++ team_creator/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse, Http404
-
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+
+import team_creator
 
 from .models import Student, TimeSlot, PM
 
@@ -21,8 +23,32 @@ def student_detail(request, student_id):
 
 def student_time_slots(request, student_id):
     response = "Student's time slots are %s."
-    return HttpResponse(response % student_id)
+    time_slots = get_object_or_404(TimeSlot.objects.all(), pk=student_id)
+    return HttpResponse(response % time_slots)
 
 
 def student_team(request, student_id):
     return HttpResponse("Student's team is %s." % student_id)
+
+
+def choose_time_slot(request, time_slot_id):
+    time_slot = get_object_or_404(TimeSlot, pk=time_slot_id)
+    try:
+        selected_choice = time_slot.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, TimeSlot.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "team_creator/detail.html",
+            {
+                "time_slot": time_slot,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.time_slot += f" {time_slot} "
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("team_creator:slots", args=(time_slot.id,)))
