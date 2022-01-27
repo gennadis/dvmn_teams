83883de9cac98682f5adfd5f11d95c9f3bd5f127
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-27 12:52:21 +0300

    Create Student model

--- dvmn_teams/settings.py
+++ dvmn_teams/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "team_creator.apps.TeamCreatorConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- team_creator/models.py
+++ team_creator/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(verbose_name="Name", max_length=200)
+    level = models.CharField(verbose_name="Level", max_length=200)
+    tg_username = models.CharField(verbose_name="Telegram username", max_length=200)
+    discord_username = models.CharField(verbose_name="Discord username", max_length=200)
+    is_far_east = models.BooleanField(
+        verbose_name="User from Far Eastern Federal District", default=False
+    )
+
+    def __str__(self) -> str:
+        return f"Student {self.name}, {self.tg_username}"
