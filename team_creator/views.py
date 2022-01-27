e32c95934e9c8659cd1ed3c4013f4b38c19f2376
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-27 14:43:22 +0300

    Update team creator index view

--- team_creator/views.py
+++ team_creator/views.py
@@ -1,8 +1,21 @@
 from django.http import HttpResponse
 
+from .models import Student, TimeSlot, PM
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the team_creator index.")
+    students_list = Student.objects.all()
+    names = ", ".join([s.name for s in students_list])
+
+    pm_list = PM.objects.all()
+    pm_names = ", ".join([pm.name for pm in pm_list])
+
+    time_slots_list = TimeSlot.objects.all()
+    time_slots = ", ".join([t.time_slot for t in time_slots_list])
+
+    output = f"Students: {names}, ||| PMs: {pm_names}, ||| Timeslots: {time_slots}"
+
+    return HttpResponse(output)
 
 
 def student_detail(request, student_id):
