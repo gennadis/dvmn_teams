a5480a228f204d35a08965265c4151cb5e4ba45e
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-27 14:24:08 +0300

    Add placeholders views

--- team_creator/urls.py
+++ team_creator/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:student_id>/", views.student_detail, name="detail"),
+    # ex: /polls/5/results/
+    path("<int:student_id>/timeslot/", views.student_time_slots, name="time slots"),
+    # ex: /polls/5/vote/
+    path("<int:student_id>/team/", views.student_team, name="team"),
 ]
--- team_creator/views.py
+++ team_creator/views.py
@@ -3,3 +3,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the team_creator index.")
+
+
+def student_detail(request, student_id):
+    return HttpResponse("You're looking at student %s." % student_id)
+
+
+def student_time_slots(request, student_id):
+    response = "Student's time slots are %s."
+    return HttpResponse(response % student_id)
+
+
+def student_team(request, student_id):
+    return HttpResponse("Student's team is %s." % student_id)
