ea467c714902acc97af5709aa9ff625fe2c78d91
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-27 17:12:59 +0300

    Add namespacing for urls

--- team_creator/urls.py
+++ team_creator/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = "team_creator"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:student_id>/", views.student_detail, name="detail"),
-    # ex: /polls/5/results/
+    # ex: /student/5/timeslot/
     path("<int:student_id>/timeslot/", views.student_time_slots, name="time slots"),
-    # ex: /polls/5/vote/
+    # ex: /student/5/team/
     path("<int:student_id>/team/", views.student_team, name="team"),
 ]
