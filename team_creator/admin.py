9ddddf53e598c2b65a377c6b598bc09db3bc8c35
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-27 13:11:08 +0300

    Add PM model

--- team_creator/admin.py
+++ team_creator/admin.py
@@ -1,8 +1,9 @@
 import site
 from django.contrib import admin
 
-from .models import Student
+from .models import PM, Student
 
 # Register your models here.
 
 admin.site.register(Student)
+admin.site.register(PM)
--- team_creator/models.py
+++ team_creator/models.py
@@ -12,3 +12,12 @@ class Student(models.Model):
 
     def __str__(self) -> str:
         return f"Student {self.name}, {self.tg_username}"
+
+
+class PM(models.Model):
+    name = models.CharField(verbose_name="Name", max_length=200)
+    tg_username = models.CharField(verbose_name="Telegram username", max_length=200)
+    discord_username = models.CharField(verbose_name="Discord username", max_length=200)
+
+    def __str__(self) -> str:
+        return f"PM {self.name}, {self.tg_username}"
