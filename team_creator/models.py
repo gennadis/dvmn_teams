8496f849b37eac815184d01a600ff5ccb001ed97
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-29 23:49:37 +0300

    Update Models

--- team_creator/models.py
+++ team_creator/models.py
@@ -1,24 +1,23 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class TimeSlot(models.Model):
-    time_slot = models.CharField(
-        verbose_name="Time slot",
+    timeslot = models.CharField(
+        verbose_name="Timeslot",
         max_length=200,
     )
 
     def __str__(self):
-        return f"Time slot {self.time_slot}"
+        return f"Timeslot {self.timeslot}"
 
 
 class PM(models.Model):
     name = models.CharField(verbose_name="Name", max_length=200)
     tg_username = models.CharField(verbose_name="Telegram username", max_length=200)
     discord_username = models.CharField(verbose_name="Discord username", max_length=200)
-    time_slots = models.ManyToManyField(
+    timeslot = models.ManyToManyField(
         TimeSlot,
-        verbose_name="Time slot",
+        verbose_name="Timeslot",
         max_length=200,
         related_name="PMs",
     )
@@ -35,13 +34,12 @@ class Student(models.Model):
     is_far_east = models.BooleanField(
         verbose_name="User from Far Eastern Federal District", default=False
     )
-    time_slot = models.ManyToManyField(
+    timeslot = models.ManyToManyField(
         TimeSlot,
-        verbose_name="Student time slot",
+        verbose_name="Student timeslot",
         related_name="Students",
         blank=True,
     )
-    in_team = models.BooleanField(verbose_name="Studetn already in team", default=False)
 
     def __str__(self) -> str:
         return f"Student {self.name}, tg: {self.tg_username}"
@@ -56,9 +54,9 @@ class Team(models.Model):
         null=True,
         blank=True,
     )
-    time_slot = models.ForeignKey(
+    timeslot = models.ForeignKey(
         TimeSlot,
-        verbose_name="Team time slot",
+        verbose_name="Team timeslot",
         related_name="Teams",
         on_delete=models.SET_NULL,
         null=True,
@@ -69,14 +67,6 @@ class Team(models.Model):
         verbose_name="Team students",
         related_name="Students",
         blank=True,
-    )
-    counter = models.IntegerField(
-        verbose_name="Students in Team",
-        validators=[
-            MinValueValidator(0),
-            MaxValueValidator(3),
-        ],
-        default=0,
     )
     level = models.CharField(
         verbose_name="Level",
