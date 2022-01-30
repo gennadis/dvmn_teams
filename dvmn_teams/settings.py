c4ab2880ae0d5455835710b9f8a5d8e1b2ebc0db
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-30 14:16:40 +0300

    Fix `ALLOWED_HOSTS`

--- dvmn_teams/settings.py
+++ dvmn_teams/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["fathomless-badlands-67822.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = [".herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
