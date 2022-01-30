01f1dcda9b436e40a85600ade002eca2c64a204d
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-30 14:22:42 +0300

    Fix https auth issues

--- dvmn_teams/settings.py
+++ dvmn_teams/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 DEBUG = True
 
 ALLOWED_HOSTS = [".herokuapp.com", "127.0.0.1"]
-
+CSRF_TRUSTED_ORIGINS = ["https://*.herokuapp.com", "https://*.127.0.0.1"]
 
 # Application definition
 
