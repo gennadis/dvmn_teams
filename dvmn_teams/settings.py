2a6245a7de669be243410c0c0ef44c2cd865366f
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-30 12:40:00 +0300

    Update `ALLOWED_HOSTS` with localhost for testing

--- dvmn_teams/settings.py
+++ dvmn_teams/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["fathomless-badlands-67822.herokuapp.com"]
+ALLOWED_HOSTS = ["fathomless-badlands-67822.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
