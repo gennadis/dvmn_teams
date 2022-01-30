3ac5243d1c136b7308b1f8b446d18d5ec4286060
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-30 13:50:43 +0300

    Automate configuration with django-heroku

--- dvmn_teams/settings.py
+++ dvmn_teams/settings.py
@@ -14,6 +14,7 @@ import os
 import environ
 from pathlib import Path
 
+
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,3 +129,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+import django_heroku
+
+django_heroku.settings(locals())
