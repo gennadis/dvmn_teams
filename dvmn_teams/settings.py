f252563027fc4d0e834300a4976b4f4874dd4a38
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-31 21:55:52 +0300

    Fix imports in `settings.py`

--- dvmn_teams/settings.py
+++ dvmn_teams/settings.py
@@ -11,11 +11,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
-import environ
 from pathlib import Path
 
+import django_heroku
+import environ
 
 environ.Env.read_env()
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,7 +131,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-import django_heroku
-
-django_heroku.settings(locals())
