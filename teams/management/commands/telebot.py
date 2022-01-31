ac1e5e4ce47bb7add34b825f74a50d49e2152688
Gennady Badinov <gennady.badinov@gmail.com>
2022-02-01 00:01:20 +0300

    Fix bot "already in team" message

--- teams/management/commands/telebot.py
+++ teams/management/commands/telebot.py
@@ -22,21 +22,21 @@ def start_message(message) -> Message:
         if not student.in_team:
             bot.send_message(
                 chat_id=message.chat.id,
-                text=f"""Привет, {student.name}!\n
-                Я помогу тебе записаться на текущий командный проект Devman.\n
-                Для продолжения введи команду /enroll""",
+                text=f"Привет, {student.name}!\n"
+                "Я помогу тебе записаться на текущий командный проект Devman.\n"
+                "Для продолжения введи команду /enroll",
             )
         else:
             bot.send_message(
                 chat_id=message.chat.id,
-                text=f"""{student.name}, ты уже записан на командный проект.\n
-                Время созвона: {student.timeslot.timeslot.first()}.""",
+                text=f"{student.name}, ты уже записан на командный проект.\n"
+                f"Время созвона: {student.timeslot.first()}.",
             )
-    except:
+    except Student.DoesNotExist:
         bot.send_message(
             chat_id=message.chat.id,
-            text="""Вероятно, ты не являешься студентом Devman.\n
-            Вступай в наши ряды по ссылке: https://dvmn.org/""",
+            text="Вероятно, ты не являешься студентом Devman."
+            "Вступай в наши ряды по ссылке: https://dvmn.org/",
         )
 
 
@@ -119,18 +119,18 @@ def callback_query(call):
             bot.edit_message_text(
                 chat_id=call.message.chat.id,
                 message_id=call.message.message_id,
-                text=f"""Отлично!\n
-                Ты записан к {user_team.pm}: {user_team.pm.tg_username} на {user_team.timeslot.timeslot}.\n
-                Студенты в твоей команде:\n
-                {', '.join([f'{student.name}: {student.tg_username}' for student in user_team.students.all()])}""",
+                text="Отлично!\n"
+                f"Ты записан к {user_team.pm}: {user_team.pm.tg_username} на {user_team.timeslot.timeslot}.\n"
+                "Студенты в твоей команде:\n"
+                f"{', '.join([f'{student.name}: {student.tg_username}' for student in user_team.students.all()])}",
                 reply_markup=None,
             )
 
     else:
         bot.send_message(
             chat_id=call.message.chat.id,
-            text=f"""{student.name}, ты уже записан на командный проект.\n
-                Время созвона: {student.timeslot.timeslot.first()}.""",
+            text=f"{student.name}, ты уже записан на командный проект.\n"
+            f"Время созвона: {student.timeslot.first()}.",
         )
 
 
