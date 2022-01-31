8964bc6f6ea59977f3cb32845f1d50522525704b
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-31 22:48:22 +0300

    Refactor Teams creation code for readability

--- teams/management/commands/maketeams.py
+++ teams/management/commands/maketeams.py
@@ -3,38 +3,46 @@ from collections import Counter
 from typing import Generator
 
 from django.core.management.base import BaseCommand
+from django.db.models import QuerySet
+
 from teams.models import TimeSlot, Student, PM, Team
 
 
-def generate_team_level() -> Generator:
-    student_levels = [s.level for s in Student.objects.all()]
+def calculate_divmod_three(num: int) -> int:
+    quotient, remainder = divmod(num, 3)
+    if remainder:
+        return quotient + 1
+    return quotient
 
-    level_count = dict(Counter(student_levels))
-    for level, num in level_count.items():
-        level_count[level] = count_divmod_three(num)
+
+def generate_team_level(students: QuerySet) -> Generator:
+    student_levels = [student.level for student in students]
+
+    levels_counter = Counter(student_levels)
+    for level, count in levels_counter.items():
+        levels_counter[level] = calculate_divmod_three(count)
 
     team_levels = []
-    for level, count in level_count.items():
+    for level, count in levels_counter.items():
         team_levels.extend([level] * count)
     random.shuffle(team_levels)
 
-    return (x for x in team_levels)
+    return (level for level in team_levels)
 
 
-def count_divmod_three(num: int) -> int:
-    quotient, remainder = divmod(num, 3)
-    return quotient + 1 if remainder else quotient
+def generate_empty_teams(students: QuerySet, pms: QuerySet) -> QuerySet:
+    team_level = generate_team_level(students)
+    for pm in pms:
+        for pm_timeslot in pm.timeslot.all():
+            Team.objects.create(pm=pm, timeslot=pm_timeslot, level=next(team_level))
 
-
-def generate_empty_teams() -> None:
-    team_level = generate_team_level()
-    for pm in PM.objects.all():
-        for ts in pm.timeslot.all():
-            Team.objects.create(pm=pm, timeslot=ts, level=next(team_level))
+    return Team.objects.all()
 
 
 class Command(BaseCommand):
-    help = "Make some teams"
+    help = "Make empty teams for each PM and his\her timeslot"
 
     def handle(self, *args, **kwargs):
-        generate_empty_teams()
+        students = Student.objects.all()
+        pms = PM.objects.all()
+        empty_teams = generate_empty_teams(students, pms)
