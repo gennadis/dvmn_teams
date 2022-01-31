12e4c1efcb22364bda38502101338d14d2756200
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-31 22:47:49 +0300

    Refactor fake fixtures generation for readability

--- teams/management/commands/makefakes.py
+++ teams/management/commands/makefakes.py
@@ -1,43 +1,39 @@
-"""Run this command to import generated fixtures:
-
-python manage.py loaddata students.json pms.json timeslots.json
-"""
-import os
 import json
+import os
 import random
 
 from django.core.management.base import BaseCommand
 from faker import Faker
 
-# timeslots
+FIXTURES_PATH = "./teams/fixtures/"
+
 START_HOUR = 8
 END_HOUR = 22
-TIMESLOTS_TOTAL = 2 * (END_HOUR - START_HOUR)
+TIMESLOTS_TOTAL = 2 * (END_HOUR - START_HOUR)  # 1 hour == 2 timeslots
 
-# students
 STUDENTS_TOTAL_COUNT = 50
-
-# pm
 PM_TOTAL_COUNT = 2
 PM_TOTAL_TIMESLOTS = 5
 
-# teams
 TEAMS_TOTAL_COUNT = int(STUDENTS_TOTAL_COUNT / 3)
 
 
-def save_json(data: list[dict], filename: str) -> None:
+def save_json(data: list[dict], filename: str) -> str:
     with open(filename, "w") as file:
         json.dump(data, file, indent=4)
 
+    return filename
 
-def split_hours(start: int, end: int) -> list:
+
+def split_hours(start_hour: int, end_hour: int) -> list:
     intervals = []
-    for hour in range(start, end):
+    for hour in range(start_hour, end_hour):
         intervals.extend([f"{hour}:00 - {hour}:30", f"{hour}:30 - {hour+1}:00"])
+
     return intervals
 
 
-def generate_timeslots_json(filename: str, start_hour: int, end_hour: int) -> str:
+def generate_timeslots_fixtures(filename: str, start_hour: int, end_hour: int) -> str:
     timeslots = []
     for count, timeslot in enumerate(split_hours(start_hour, end_hour), start=1):
         timeslots.append(
@@ -48,10 +44,11 @@ def generate_timeslots_json(filename: str, start_hour: int, end_hour: int) -> st
             }
         )
     save_json(timeslots, filename)
+
     return filename
 
 
-def generate_pm_json(filename: str, count: int, fake: Faker) -> str:
+def generate_pm_fixtures(filename: str, count: int, fake: Faker) -> str:
     pms = []
     for i in range(1, count + 1):
         pms.append(
@@ -60,18 +57,19 @@ def generate_pm_json(filename: str, count: int, fake: Faker) -> str:
                 "pk": i,
                 "fields": {
                     "name": fake.name(),
-                    "tg_username": f"@{fake.word()}_{fake.word()}",
-                    "discord_username": f"@{fake.word()}#1234",
+                    "tg_username": f"@{fake.word()}_{fake.word()}",  # @word_word
+                    "discord_username": f"{fake.word()}#{random.randint(1_000, 10_000)}",  # word#9876
                     # 18:00 - 22:00 timeslots for all PMs
                     "timeslot": [21, 22, 23, 24, 25, 26, 27, 28],
                 },
             }
         )
     save_json(pms, filename)
+
     return filename
 
 
-def generate_students_json(filename: str, count: int, fake: Faker) -> str:
+def generate_students_fixtures(filename: str, count: int, fake: Faker) -> str:
     students = []
     for i in range(1, count + 1):
         students.append(
@@ -81,8 +79,8 @@ def generate_students_json(filename: str, count: int, fake: Faker) -> str:
                 "fields": {
                     "name": fake.name(),
                     "level": random.choice(["novice", "novice+", "junior"]),
-                    "tg_username": f"@{fake.word()}_{fake.word()}",
-                    "discord_username": f"{fake.word()}#{random.randint(1_000, 10_000)}",
+                    "tg_username": f"@{fake.word()}_{fake.word()}",  # @word_word
+                    "discord_username": f"{fake.word()}#{random.randint(1_000, 10_000)}",  # word#9876
                     "is_far_east": random.choice([True, False]),
                     "timeslot": [],
                     "in_team": False,
@@ -90,6 +88,7 @@ def generate_students_json(filename: str, count: int, fake: Faker) -> str:
             }
         )
     save_json(students, filename)
+
     return filename
 
 
@@ -98,10 +97,14 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         fake = Faker()
-        os.makedirs("./teams/fixtures/", exist_ok=True)
-        generate_timeslots_json("./teams/fixtures/timeslots.json", START_HOUR, END_HOUR)
-        generate_pm_json("./teams/fixtures/pms.json", PM_TOTAL_COUNT, fake)
-        # generate_teams_json("./teams/fixtures/teams.json", TEAMS_TOTAL_COUNT)
-        generate_students_json(
-            "./teams/fixtures/students.json", STUDENTS_TOTAL_COUNT, fake
+        os.makedirs(FIXTURES_PATH, exist_ok=True)
+
+        generate_timeslots_fixtures(
+            os.path.join(FIXTURES_PATH, "timeslots.json"), START_HOUR, END_HOUR
+        )
+        generate_pm_fixtures(
+            os.path.join(FIXTURES_PATH, "pms.json"), PM_TOTAL_COUNT, fake
+        )
+        generate_students_fixtures(
+            os.path.join(FIXTURES_PATH, "students.json"), STUDENTS_TOTAL_COUNT, fake
         )
